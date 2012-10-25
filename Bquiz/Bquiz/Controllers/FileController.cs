3635944002acc3ea2c2084af18367f0a3545b2df
Tư Cuồng <wikiblack713@gmail.com>
2012-10-25 13:06:45 +0700

    added Bquiz project

---
+++ Bquiz/Bquiz/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Bquiz.Models;
+using System.IO;
+
+namespace Bquiz.Controllers
+{
+    public class FileController : Controller
+    {
+        BluequizEntities db = new BluequizEntities();
+
+        //
+        // GET: /File/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadQuestionGroupAudio(IEnumerable<HttpPostedFileBase> attachments, Guid questionGroupId)
+        {
+            // The Name of the Upload component is "attachments" 
+            foreach (var file in attachments)
+            {
+                // Some browsers send file names with full path. We only care about the file name.
+                var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                var destinationPath = Path.Combine(Server.MapPath("~/Content/Audio"), fileName);
+
+                file.SaveAs(destinationPath);
+                db.bq_QuestionGroup_UpdateMediaPath(questionGroupId, fileName);
+            }
+
+            // Return an empty string to signify success
+            return Content("");
+        }
+
+    }
+}
