0653391f81ce0addab0e5dd7849d0b5a1b97443c
Tư Cuồng <wikiblack713@gmail.com>
2012-11-01 23:37:32 +0700

    Project chuẩn

--- Bquiz/Bquiz/Controllers/Bq_HauController.cs
+++ Bquiz/Bquiz/Controllers/Bq_HauController.cs
@@ -22,49 +22,14 @@ namespace Bquiz.Controllers
         //Viết vào cái này nhé
         public ActionResult UpdatePart1(Guid groupId)
         {
-            var group = db.bq_QuestionGroup_GetById(groupId).Single();
-            var questionList = db.bq_Question_GetByQuestionGroupId(group.QuestionGroupId).ToList();
-
-            bq_part1 part1 = new bq_part1();
-            part1.Group = group;
-            part1.QuestionList = questionList;
             return View();
         }
 
 
         //Viết vào cái này nhé
-        [HttpPost]
-        public ActionResult UpdatePart1(bq_part1 part1)
+        public ActionResult UpdatePart2s(Guid groupId)
         {
-            if (ModelState.IsValid)
-            {
-                db.bq_QuestionGroup_Update(
-                    part1.Group.QuestionGroupId,
-                    part1.Group.Name,
-                    part1.Group.Order,
-                    part1.Group.Paragraph,
-                    part1.Group.MediaPath);
-
-                foreach (var question in part1.QuestionList)
-                {
-                    db.bq_Question_Update(
-                        question.QuestionId,
-                        question.Order,
-                        question.ImagePath,
-                        question.MediaPath,
-                        question.Paragraph,
-                        question.OptionA,
-                        question.OptionB,
-                        question.OptionC,
-                        question.OptionD,
-                        question.Answer);
-                }
-
-                //Trả về trang chủ
-                return Redirect("/");
-            }
-            else 
-                return View();
+            return View();
         }
     }
 }
